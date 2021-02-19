5d79bbefdb58740f0a49f279a9d68ec0b5ddfa44
Markus Zoppelt <markus@zoppelt.net>
2021-02-19 17:41:53 +0100

    turret is working now. closes #16

--- Assets/Scripts/Enemies/TurretEnemyAttack.cs
+++ Assets/Scripts/Enemies/TurretEnemyAttack.cs
@@ -7,16 +7,26 @@ public class TurretEnemyAttack : BaseEnemyAttack
     [SerializeField] internal Bullet bulletPrefab;
     [SerializeField] internal Transform muzzle;
     [SerializeField] internal float bulletSpeed;
+    internal float coolDownTime;
+
+    public void Update()
+    {
+        if (coolDownTime > 0)
+            coolDownTime -= Time.deltaTime;
+    }
 
     public override void Attack(Destructable target)
     {
-        Debug.Log("Attacking... spawning bullet");
+        if (coolDownTime > 0)
+            return;
+
         Bullet bullet = Instantiate(bulletPrefab, muzzle.position, Quaternion.identity);
 
         bullet.damage = this.damage;
         bullet.speed = bulletSpeed;
         bullet.direction = (target.transform.position - muzzle.position).normalized;
         bullet.direction.y = 0;
+        coolDownTime = this.timeBetweenAttacks;
     }
 
     public override Destructable CalculateTarget(GameObject[] targets) {
