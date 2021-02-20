4d13ed68acbc55703dffa59124a3df26cc96ac1c
Twinf0x <bastian.7741@gmail.com>
2021-02-20 15:11:37 +0100

    Added audio controls to the main menu. Fixed an error with the audio settings.

--- Assets/Scripts/Menus/MainMenuController.cs
+++ Assets/Scripts/Menus/MainMenuController.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] private GameObject morePanel;
     [SerializeField] private GameObject mainButtonGroup;
+    [SerializeField] private Image musicButtonImage;
+    [SerializeField] private Image sfxButtonImage;
+    [SerializeField] private AudioManager audioManager;
+    [Header("UI Sprites")]
+    [SerializeField] private Sprite musicOn;
+    [SerializeField] private Sprite musicOff;
+    [SerializeField] private Sprite sfxOn;
+    [SerializeField] private Sprite sfxOff;
 
     public void ExitGame()
     {
@@ -32,5 +41,34 @@ public class MainMenuController : MonoBehaviour
     public void FollowLink(string url)
     {
         Application.OpenURL(url);
+    }
+
+    public void ToggleAudio(string groupName)
+    {
+        audioManager.ToggleMute(groupName);
+    }
+
+    public void UpdateMusicButton()
+    {
+        if (audioManager.IsMuted("Music"))
+        {
+            musicButtonImage.sprite = musicOff;
+        }
+        else
+        {
+            musicButtonImage.sprite = musicOn;
+        }
+    }
+
+    public void UpdateSFXButton()
+    {
+        if (audioManager.IsMuted("SFX"))
+        {
+            sfxButtonImage.sprite = sfxOff;
+        }
+        else
+        {
+            sfxButtonImage.sprite = sfxOn;
+        }
     }
 }
